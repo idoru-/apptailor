403678ea4593ae7e9759f1ca135f0b598020a402
idoru- <idoru.maria@gmail.com>
2019-09-03 15:56:50 -0700

    create settings

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-import { Box, Heading } from 'grommet'
+import { Box, Button, CheckBox, Heading, Text } from 'grommet'
+
+function NotificationsCheckbox() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <CheckBox
+      checked={checked}
+      label='Receive Push Notifications'
+      onChange={event => setChecked(event.target.checked)}
+    />
+  )
+}
 
 export const Settings = () => (
   <Box fill align='center' justify='center'>
     <Heading>Settings</Heading>
+    <Box>{NotificationsCheckbox()}</Box>
+    <Heading level='3'>Linked Accounts</Heading>
+    <Text>Calendar</Text>
+    <Text>Facebook</Text>
+    <Button label='Link New Account' href='#' />
+    <Heading level='3'>Delete Account</Heading>
+    <Text>This action is forever.</Text>
+    <Button label='Delete' href='#' />
   </Box>
 )
