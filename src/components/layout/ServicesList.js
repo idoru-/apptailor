03b5922ea6048b8b32333b1725000a4c2439ec9b
idoru- <idoru.maria@gmail.com>
2019-09-06 22:34:44 -0700

    extend service/product card list item

--- src/components/layout/ServicesList.js
+++ src/components/layout/ServicesList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Box, Button, Grid, InfiniteScroll, Image, Text, Heading } from 'grommet'
+import { Box, Button, Grid, InfiniteScroll, Image, Paragraph, Heading } from 'grommet'
 
 const services = [
   {
@@ -9,6 +9,8 @@ const services = [
     duration: '50 min',
     image: '/img/louis-hansel-e96ST3p7tn4-unsplash.jpg',
     id: '1',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti alias nostrum itaque voluptates veritatis quibusdam officiis dignissimos iusto accusamus possimus.',
   },
   {
     name: 'Service Name Number II',
@@ -16,6 +18,7 @@ const services = [
     duration: '50 min',
     image: '/img/joanne-caselyn-suarez-kCGhXLU32Bg-unsplash.jpg',
     id: '2',
+    discription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, odio.',
   },
   {
     name: 'Service Name Number III',
@@ -23,6 +26,8 @@ const services = [
     duration: '50 min',
     image: '/img/james-fitzgerald-h2Sp-CFF9Jc-unsplash.jpg',
     id: '3',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number IV',
@@ -30,6 +35,8 @@ const services = [
     duration: '50 min',
     image: '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
     id: '4',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number V',
@@ -37,6 +44,8 @@ const services = [
     duration: '50 min',
     image: '/img/mungyu-kim-Ex57cKpwdnE-unsplash.jpg',
     id: '5',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number VI',
@@ -44,6 +53,8 @@ const services = [
     duration: '50 min',
     image: '/img/micheile-henderson-IIEZ3Q0q2T4-unsplash.jpg',
     id: '6',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number VII',
@@ -51,6 +62,8 @@ const services = [
     duration: '50 min',
     image: '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
     id: '7',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number VIII',
@@ -58,6 +71,8 @@ const services = [
     duration: '50 min',
     image: '/img/micheile-henderson-IIEZ3Q0q2T4-unsplash.jpg',
     id: '8',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number IX',
@@ -65,6 +80,8 @@ const services = [
     duration: '50 min',
     image: '/img/kuan-fang-w6BD_RZHnDs-unsplash.jpg',
     id: '9',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number X',
@@ -72,6 +89,8 @@ const services = [
     duration: '50 min',
     image: '/img/joanne-caselyn-suarez-kCGhXLU32Bg-unsplash.jpg',
     id: '10',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number XI',
@@ -79,6 +98,8 @@ const services = [
     duration: '50 min',
     image: '/img/fas-khan-n-DPU28GoEA-unsplash.jpg',
     id: '11',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number XII',
@@ -86,6 +107,8 @@ const services = [
     duration: '50 min',
     image: '/img/dylan-nolte--D09rH5sTw4-unsplash.jpg',
     id: '12',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
   {
     name: 'Service Name Number XIII',
@@ -93,6 +116,8 @@ const services = [
     duration: '50 min',
     image: '/img/anna-earl-J9cvIUrojUY-unsplash.jpg',
     id: '13',
+    discription:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
   },
 ]
 
@@ -100,17 +125,22 @@ export default class ServicesList extends Component {
   render() {
     return (
       <Box height='medium' width='large' overflow='auto'>
-        <Grid columns='small' rows='small' pad='xsmall' gap='small'>
+        <Grid columns='small' rows='medium' pad='xsmall' gap='small'>
           <InfiniteScroll items={services}>
             {item => (
-              <Box flex={false} pad='xsmall' background='dark-1' key={item.id}>
+              <Box flex={false} background='dark-1' key={item.id}>
                 <Image src={item.image} fit='cover' />
-                <Heading level='5' margin='none'>
-                  {item.name}
-                </Heading>
-                <Button>
-                  <strong>+</strong>
-                </Button>
+                <Box pad='small'>
+                  <Heading level='5' margin='none'>
+                    {item.name}
+                  </Heading>
+                  <Paragraph size='small'>{item.discription}</Paragraph>
+                  <Button color='accent-2' margin={{ horizontal: 'auto' }}>
+                    <Heading level='3' margin={{ vertical: 'none' }}>
+                      <strong>SELECT</strong>
+                    </Heading>
+                  </Button>
+                </Box>
               </Box>
             )}
           </InfiniteScroll>
