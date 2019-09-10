ac336e7dc93ef1721beee83c4c6c1a2f86cbdb84
idoru- <idoru.maria@gmail.com>
2019-09-09 22:06:47 -0700

    add sample data for Carousel

--- src/components/layout/ServicesList.js
+++ src/components/layout/ServicesList.js
@@ -7,7 +7,11 @@ const services = [
     name: 'Service Name Number I',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/louis-hansel-e96ST3p7tn4-unsplash.jpg',
+    images: [
+      '/img/louis-hansel-e96ST3p7tn4-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '1',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti alias nostrum itaque voluptates veritatis quibusdam officiis dignissimos iusto accusamus possimus.',
@@ -16,7 +20,11 @@ const services = [
     name: 'Service Name Number II',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/joanne-caselyn-suarez-kCGhXLU32Bg-unsplash.jpg',
+    images: [
+      '/img/joanne-caselyn-suarez-kCGhXLU32Bg-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '2',
     discription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, odio.',
   },
@@ -24,7 +32,11 @@ const services = [
     name: 'Service Name Number III',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/james-fitzgerald-h2Sp-CFF9Jc-unsplash.jpg',
+    images: [
+      '/img/james-fitzgerald-h2Sp-CFF9Jc-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '3',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -33,7 +45,11 @@ const services = [
     name: 'Service Name Number IV',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+    images: [
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '4',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -42,16 +58,23 @@ const services = [
     name: 'Service Name Number V',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/mungyu-kim-Ex57cKpwdnE-unsplash.jpg',
+    images: [
+      '/img/mungyu-kim-Ex57cKpwdnE-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '5',
-    discription:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
+    discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     name: 'Service Name Number VI',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/micheile-henderson-IIEZ3Q0q2T4-unsplash.jpg',
+    images: [
+      '/img/micheile-henderson-IIEZ3Q0q2T4-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '6',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -60,7 +83,11 @@ const services = [
     name: 'Service Name Number VII',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    images: [
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '7',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -69,7 +96,11 @@ const services = [
     name: 'Service Name Number VIII',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/micheile-henderson-IIEZ3Q0q2T4-unsplash.jpg',
+    images: [
+      '/img/micheile-henderson-IIEZ3Q0q2T4-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '8',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -78,7 +109,11 @@ const services = [
     name: 'Service Name Number IX',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/kuan-fang-w6BD_RZHnDs-unsplash.jpg',
+    images: [
+      '/img/kuan-fang-w6BD_RZHnDs-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '9',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -87,7 +122,11 @@ const services = [
     name: 'Service Name Number X',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/joanne-caselyn-suarez-kCGhXLU32Bg-unsplash.jpg',
+    images: [
+      '/img/joanne-caselyn-suarez-kCGhXLU32Bg-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '10',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -96,7 +135,11 @@ const services = [
     name: 'Service Name Number XI',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/fas-khan-n-DPU28GoEA-unsplash.jpg',
+    images: [
+      '/img/fas-khan-n-DPU28GoEA-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '11',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -105,7 +148,11 @@ const services = [
     name: 'Service Name Number XII',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/dylan-nolte--D09rH5sTw4-unsplash.jpg',
+    images: [
+      '/img/dylan-nolte--D09rH5sTw4-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '12',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
@@ -114,7 +161,11 @@ const services = [
     name: 'Service Name Number XIII',
     price: '$ 200',
     duration: '50 min',
-    image: '/img/anna-earl-J9cvIUrojUY-unsplash.jpg',
+    images: [
+      '/img/anna-earl-J9cvIUrojUY-unsplash.jpg',
+      '/img/henk-van-der-steege-sJvE2oIWicA-unsplash.jpg',
+      '/img/luigi-pozzoli-hzquWrcAwQ4-unsplash.jpg',
+    ],
     id: '13',
     discription:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptate maxime nulla reiciendis tenetur. Recusandae.',
