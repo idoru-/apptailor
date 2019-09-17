b1956875ac4f11114800cf39c6341c671ebb5aaf
Alan Daud <olancheg@gmail.com>
2019-09-16 22:46:03 -0700

    fix footer tag

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,32 +3,67 @@ import { Box, Button, Heading, Anchor, Text } from 'grommet'
 
 export const Footer = ({ appName }) => (
   <Box
-    tag='header'
+    tag='footer'
     direction='row'
     align='center'
     justify='between'
     background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    pad={{
+      left: 'medium',
+      right: 'small',
+      vertical: 'small',
+    }}
     elevation='medium'
-    style={{ zIndex: '1' }}
+    style={{
+      zIndex: '1',
+    }}
   >
     <Box direction='row' align='center'>
-      <Heading level='4' margin={{ right: 'small', vertical: 'none' }}>
-        {appName} © 2019
+      <Heading
+        level='4'
+        margin={{
+          right: 'small',
+          vertical: 'none',
+        }}
+      >
+        {appName}© 2019
       </Heading>
     </Box>
     <Box direction='row' align='center'>
-      <Anchor href='/signup' margin={{ right: 'small' }}>
+      <Anchor
+        href='/signup'
+        margin={{
+          right: 'small',
+        }}
+      >
         Sign Up
       </Anchor>
       <Text>|</Text>
-      <Anchor href='/signin' margin={{ left: 'small' }}>
+      <Anchor
+        href='/signin'
+        margin={{
+          left: 'small',
+        }}
+      >
         Sign In
       </Anchor>
     </Box>
     <Button href='/support'>
-      <Box flex={false} direction='row' align='center' margin={{ left: 'small' }}>
-        <Anchor level='4' margin={{ right: 'small', vertical: 'none' }}>
+      <Box
+        flex={false}
+        direction='row'
+        align='center'
+        margin={{
+          left: 'small',
+        }}
+      >
+        <Anchor
+          level='4'
+          margin={{
+            right: 'small',
+            vertical: 'none',
+          }}
+        >
           Support
         </Anchor>
       </Box>
