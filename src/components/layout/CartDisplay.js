ead3b422aff82022fb9010f45604661d4f6ee9ff
idoru- <idoru.maria@gmail.com>
2019-09-22 19:44:09 -0700

    fix link

--- src/components/layout/CartDisplay.js
+++ src/components/layout/CartDisplay.js
@@ -6,8 +6,9 @@ export const CartDisplay = () => (
   <Box direction='row-reverse' pad={{ horizontal: 'medium' }}>
     <Anchor
       icon={<BasketIcon size='medium' />}
+      href='/cart'
       label={
-        <Heading level='4' href='/cart' margin={{ horizontal: 'small' }}>
+        <Heading level='4' margin={{ horizontal: 'small' }}>
           My&nbsp;Cart&nbsp;(0)
         </Heading>
       }
