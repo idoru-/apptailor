b0f7b65f399772a2def2ea94db26b79eb4c5bebc
idoru- <idoru.maria@gmail.com>
2019-09-24 12:45:34 -0700

    refine Settings page

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -10,24 +10,29 @@ function NotificationsCheckbox() {
       checked={checked}
       label='Receive Push Notifications'
       onChange={event => setChecked(event.target.checked)}
+      toggle
     />
   )
 }
 
 export const Settings = () => (
-  <Box fill align='center' justify='center'>
-    <Heading>Settings</Heading>
-    <Box>{NotificationsCheckbox()}</Box>
-    <Heading level='3'>Linked Accounts</Heading>
-    <Text>
-      <LinkIcon color='brand' /> Calendar
-    </Text>
-    <Text>
-      <LinkIcon color='brand' /> Facebook
-    </Text>
-    <Button label='Link New Account' href='#' margin='small' />
-    <Heading level='3'>Delete Account</Heading>
-    <Text>This action is forever.</Text>
-    <Button label='Delete' href='#' margin='small' />
+  <Box fill align='center' gap='large'>
+    <Heading margin={{ top: 'xlarge' }}>Settings</Heading>
+    <Box align='center'>{NotificationsCheckbox()}</Box>
+    <Box align='center'>
+      <Heading level='3'>Linked Accounts</Heading>
+      <Text>
+        <LinkIcon color='brand' /> Calendar
+      </Text>
+      <Text>
+        <LinkIcon color='brand' /> Facebook
+      </Text>
+      <Button label='Link New Account' href='#' margin='small' />
+    </Box>
+    <Box align='center'>
+      <Heading level='3'>Delete Account</Heading>
+      <Text>This action is forever.</Text>
+      <Button label='Delete' href='#' margin='small' />
+    </Box>
   </Box>
 )
