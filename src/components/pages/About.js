8c6a665235030a908f7fcb14f97306bd2c4c7342
idoru- <idoru.maria@gmail.com>
2019-09-30 21:13:42 -0700

    refine About Company page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
-import { Anchor, Box, Button, Text, Heading, Paragraph } from 'grommet'
+import { Anchor, Box, Button, Text, Heading, Paragraph, Table, TableRow, TableCell } from 'grommet'
 
 export const About = props => {
   props.setHeader('About Company')
 
   return (
-    <Box fill align='center' justify='start' gap='xlarge' background='brand'>
+    <Box fill justify='start' gap='xlarge' background='brand'>
       <Box align='center'>
         <Heading level='3'>Working Hours</Heading>
-        <Text>Sun - Wed: 9 AM - 8 PM</Text>
-        <Text>Thur - Sat: 9 AM - 9 PM</Text>
+        <Table>
+          <TableRow>
+            <TableCell>Sun - Wed:</TableCell>
+            <TableCell>12 AM</TableCell>
+            <TableCell> - </TableCell>
+            <TableCell>8 PM</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Thur - Sat:</TableCell>
+            <TableCell>9 AM</TableCell>
+            <TableCell> - </TableCell>
+            <TableCell>8 PM</TableCell>
+          </TableRow>
+        </Table>
         <Heading level='3'>Location</Heading>
         <Text>
           1 Dr Carlton B Goodlett Pl,
